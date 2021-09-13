b5689f282c07780783c6bdf3b293b41f775a68c4
Claudio Ferraro <claudio@justbetter.nl>
2021-09-13 16:39:43 +0200

    Added option to use environment variables for different app modes

--- mk/scripts/magepack-generate.sh
+++ mk/scripts/magepack-generate.sh
@@ -3,6 +3,8 @@
 # Include all variables from .env file.
 [ -f app/etc/.env ] && source app/etc/.env
 [ -f .env ] && source .env
+[ -f app/etc/.env.${APP_ENV} ] && source app/etc/.env.${APP_ENV}
+[ -f .env.${APP_ENV} ] && source .env.${APP_ENV}
 
 # Verify required values are available.
 required_values=(CMS_URL CATEGORY_URL PRODUCT_URL)
