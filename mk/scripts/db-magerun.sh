d35175cfe435866ca1f31471769db70d0dd1217b
indykoning <15870933+indykoning@users.noreply.github.com>
2024-02-05 13:22:29 +0100

    Exclude Xtento history & logs + sync_log

--- mk/scripts/db-magerun.sh
+++ mk/scripts/db-magerun.sh
@@ -14,7 +14,7 @@ for required_value in ${required_values[@]}; do
 done
 
 # Use MAGERUN_STRIP value, if it does not exist use default values
-MAGERUN_STRIP="${MAGERUN_STRIP:-@2fa @aggregated @customers @idx @log @oauth @quotes @replica @sales @search @sessions @stripped @trade @temp amasty_xsearch_users_search}"
+MAGERUN_STRIP="${MAGERUN_STRIP:-@2fa @aggregated @customers @idx @log @oauth @quotes @replica @sales @search @sessions @stripped @trade @temp amasty_xsearch_users_search xtento_*_profile_history xtento_*_log sync_log}"
 REMOTE_SERVER_PORT="${REMOTE_SERVER_PORT:-22}"
 REMOTE_MAGERUN="${REMOTE_MAGERUN:-magerun2}"
 if [ "$FORCE_ONLINE_MAGERUN" = true ] ; then
