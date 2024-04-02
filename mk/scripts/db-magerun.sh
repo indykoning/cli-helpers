66b9836d6e65c90e54f81918c27a9e72c9acd99c
indykoning <15870933+indykoning@users.noreply.github.com>
2024-04-02 14:07:14 +0200

    Add env variable for local magerun

--- mk/scripts/db-magerun.sh
+++ mk/scripts/db-magerun.sh
@@ -17,10 +17,11 @@ done
 MAGERUN_STRIP="${MAGERUN_STRIP:-@2fa @aggregated @customers @idx @log @oauth @quotes @replica @sales @search @sessions @stripped @trade @temp amasty_xsearch_users_search xtento_*_profile_history xtento_*_log sync_log}"
 REMOTE_SERVER_PORT="${REMOTE_SERVER_PORT:-22}"
 REMOTE_MAGERUN="${REMOTE_MAGERUN:-magerun2}"
+LOCAL_MAGERUN="${LOCAL_MAGERUN:-magerun2}"
 if [ "$FORCE_ONLINE_MAGERUN" = true ] ; then
     REMOTE_MAGERUN="curl https://files.magerun.net/n98-magerun2-latest.phar -s --output /tmp/magerun2 > /dev/null && ${REMOTE_PHP:-php} /tmp/magerun2"
 fi
 
 echo -e "SSH connection info: \033[1;33mssh -p $REMOTE_SERVER_PORT $REMOTE_SERVER_USER@$REMOTE_SERVER_IP\033[0m"
 echo -e "Running \033[1;33m${REMOTE_MAGERUN} db:dump --strip=\"${MAGERUN_STRIP}\" --exclude=\"${MAGERUN_EXCLUDE}\"\033[0m and importing..."
-ssh -p $REMOTE_SERVER_PORT $REMOTE_SERVER_USER@$REMOTE_SERVER_IP "cd ${REMOTE_SERVER_PATH}; ${REMOTE_MAGERUN} db:dump --strip=\"${MAGERUN_STRIP}\" --exclude=\"${MAGERUN_EXCLUDE}\" --stdout" | magerun2 db:import /dev/stdin
+ssh -p $REMOTE_SERVER_PORT $REMOTE_SERVER_USER@$REMOTE_SERVER_IP "cd ${REMOTE_SERVER_PATH}; ${REMOTE_MAGERUN} db:dump --strip=\"${MAGERUN_STRIP}\" --exclude=\"${MAGERUN_EXCLUDE}\" --stdout" | ${LOCAL_MAGERUN} db:import /dev/stdin
