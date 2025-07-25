711b135145f9b1b4f1d9457b1761fb0b2a1682ca
Bob <bob@justbetter.nl>
2025-07-25 15:14:32 +0200

    Statamic media link

--- mk/scripts/dev-tunnel.sh
+++ mk/scripts/dev-tunnel.sh
@@ -13,10 +13,14 @@ then
     exit
 fi
 
-if [ -d "./public/wp" ]; then DIR="public/app/uploads"; fi
-if [ -f "./bin/magento" ]; then DIR="pub/media"; fi
+if [ -d "./public/wp" ]; then DIRS=("public/app/uploads"); fi
+if [ -f "./bin/magento" ]; then DIRS=("pub/media"); fi
 
-[ -z ${DIR} ] && echo "Project not compatible." && exit
+if [ -f "./please" ]; then
+    DIRS=("public/img" "public/assets")
+fi
+
+[ -z "${DIRS[*]}" ] && echo "Project not compatible." && exit
 
 # Verify required values are available.
 required_values=(REMOTE_SERVER_IP REMOTE_SERVER_USER REMOTE_SERVER_PATH)
@@ -24,41 +28,47 @@ for required_value in ${required_values[@]}; do
     [ -z ${!required_value} ] && echo "ERROR: ${required_value} is missing!" && exit
 done
 
-# Set local settings
-LOCAL_FOLDER="${LOCAL_FOLDER:-./$DIR}"
+for DIR in "${DIRS[@]}"; do
+    
+     # Set local settings
+    LOCAL_FOLDER="./$DIR"
+    TIMESTAMP=$( date +%s )
 
-TIMESTAMP=$( date +%s )
-
-
-# Setup SSHFS
-if mount | grep "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP" > /dev/null; then
-    echo "ERROR: Remote images were already mounted."
-else
-    # Check if the path is valid and get the full path
-    full_path=$(ssh $REMOTE_SERVER_USER@$REMOTE_SERVER_IP "cd ${REMOTE_SERVER_PATH}/${DIR} > /dev/null 2>&1 && pwd -P")
-
-    [ $? -ne 0 ] && echo "ERROR: There may be a typo in the remote path (don't include $DIR)" && exit;
-
-    # Check if local folder is empty
-    if [ "$(ls -A $LOCAL_FOLDER)" ]; then
-
-        # Ask user how to continue, default exit
-        echo "WARNING: Local folder not empty, move to backup? (y/N)"
-        read str
-        if [[ $str == *y* ]]; then
-
-            LOCAL_FOLDER_BACKUP_NAME="$LOCAL_FOLDER-$TIMESTAMP"
-            echo "INFO: Moving to $LOCAL_FOLDER_BACKUP_NAME"
-            mv $LOCAL_FOLDER $LOCAL_FOLDER_BACKUP_NAME
-            mkdir  $LOCAL_FOLDER
-
-        else
-            # No valid response, exiting
-            exit
-        fi
-
+    if [ ! -d "$LOCAL_FOLDER" ]; then
+        mkdir -p "$LOCAL_FOLDER"
     fi
 
-    sshfs -o allow_other,defer_permissions "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP:${full_path}" $LOCAL_FOLDER -o allow_other
-    echo "INFO: Remote images mounted"
-fi
+
+    # Setup SSHFS
+    if mount | grep "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP" | grep "$DIR" > /dev/null; then
+        echo "ERROR: Remote images were already mounted."
+    else
+        # Check if the path is valid and get the full path
+        full_path=$(ssh $REMOTE_SERVER_USER@$REMOTE_SERVER_IP "cd ${REMOTE_SERVER_PATH}/${DIR} > /dev/null 2>&1 && pwd -P")
+
+        [ $? -ne 0 ] && echo "ERROR: There may be a typo in the remote path (don't include $DIR)" && exit;
+
+        # Check if local folder is empty
+        if [ "$(ls -A $LOCAL_FOLDER)" ]; then
+
+            # Ask user how to continue, default exit
+            echo "WARNING: Local folder not empty, move to backup? (y/N)"
+            read str
+            if [[ $str == *y* ]]; then
+
+                LOCAL_FOLDER_BACKUP_NAME="$LOCAL_FOLDER-$TIMESTAMP"
+                echo "INFO: Moving to $LOCAL_FOLDER_BACKUP_NAME"
+                mv $LOCAL_FOLDER $LOCAL_FOLDER_BACKUP_NAME
+                mkdir  $LOCAL_FOLDER
+
+            else
+                # No valid response, exiting
+                exit
+            fi
+
+        fi
+
+        sshfs -o allow_other,defer_permissions "$REMOTE_SERVER_USER@$REMOTE_SERVER_IP:${full_path}" $LOCAL_FOLDER -o allow_other
+        echo "INFO: Remote images mounted"
+    fi
+done
